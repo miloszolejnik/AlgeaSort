7b2de4a389920a25d1b72643b8aa9804d5612f44
Milosz Olejnik <milosz.olejnik@tuta.io>
2024-06-20 19:29:47 +0200

    add quick styling to navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,23 @@
+import styled from "styled-components"
+
 function Nav(){
     return(
-        <div>
-            I'm a Nav Bar!
-        </div>
+        <StyledHeader>
+            <h1>Algea-Sort</h1>
+        </StyledHeader>
     )
 }
+
+const StyledHeader = styled.header`
+    background-color: rgb(249, 253, 250);
+    padding: 1rem 1rem 1rem 1rem;
+    font-size: x-small;
+    font-family: "Lobster Two", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    box-shadow: 1px 1px 30px -1px rgba(0,0,0,0.49);
+    -webkit-box-shadow: 1px 1px 30px -1px rgba(0,0,0,0.49);
+    -moz-box-shadow: 1px 1px 30px -1px rgba(0,0,0,0.49);
+`
 
 export default Nav
